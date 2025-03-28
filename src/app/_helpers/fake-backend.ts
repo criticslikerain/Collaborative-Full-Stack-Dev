0429a344a21641e6b5969178740d9c0508248f5c
themarneilx <joncabs200@gmail.com>
2025-03-28 15:32:16 +0800

    fixed some filess

--- src/app/_helpers/fake-backend.ts
+++ src/app/_helpers/fake-backend.ts
@@ -3,8 +3,8 @@ import { HttpRequest, HttpResponse, HttpHandler, HttpEvent, HttpInterceptor, HTT
 import { Observable, of, throwError } from 'rxjs';
 import { delay, materialize, dematerialize } from 'rxjs/operators';
 
-import { AlertService } from '@app/_services';
-import { Role } from '@app/models';
+import { AlertService } from '../_services';
+import { Role } from '../models';
 
 //array in local storage for accounts
 const accountsKey = 'angular-10-signup-verification-boilerplate-accounts';
